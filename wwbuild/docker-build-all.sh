d57ee9f2d5d425c3f01efdabff37c6e8da2b7dfe
peter-hartmann-emrsn <peter.hartmann@emerson.com>
2019-01-23 16:26:30 -0600

    mend docker-buildall

--- wwbuild/docker-build-all.sh
+++ wwbuild/docker-build-all.sh
@@ -1,4 +1,4 @@
 
 echo ""
 echo "Building source code using docker container"
-docker run -it --rm --name willywonka -v "%cd%./../:/root/WillyWonka" http://usaust-edge.com:5000/willywonka
+docker run -it --rm --name willywonka -v "C:\Users\phartmann\Desktop\git\WillyWonka:/WillyWonka" docker.usaust-edge.com/willywonka
