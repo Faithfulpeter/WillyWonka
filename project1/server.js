cb6179a39a29b0e8705d2166a1c5d5eb51d59986
peter-hartmann-emrsn <peter.hartmann@emerson.com>
2019-01-23 14:10:19 -0600

    add project2

--- project1/server.js
+++ project1/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 // Constants
 const PORT = 8080;
@@ -8,6 +9,9 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../project2/build')));
+
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
