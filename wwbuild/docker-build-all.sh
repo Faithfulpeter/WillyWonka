62651ed7732b4dc47b4e9cbe16206ac8790db0fa
HarleyFreeman <46970422+HarleyFreeman@users.noreply.github.com>
2019-01-23 16:52:43 -0600

    Modifying the script to use the "current directory" DOS macro

--- wwbuild/docker-build-all.sh
+++ wwbuild/docker-build-all.sh
@@ -1,4 +1,4 @@
 
 echo ""
 echo "Building source code using docker container"
-docker run -it --rm --name willywonka -v "C:\Users\phartmann\Desktop\git\WillyWonka:/WillyWonka" docker.usaust-edge.com/willywonka
+docker run -it --rm --name willywonka -v "%cd%\..:/WillyWonka" docker.usaust-edge.com/willywonka
