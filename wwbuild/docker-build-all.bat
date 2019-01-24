5c31d301e5698505783cecbb1fa3d307dafe76e6
peter-hartmann-emrsn <peter.hartmann@emerson.com>
2019-01-24 09:17:40 -0600

    build dockercontainer locally

--- wwbuild/docker-build-all.bat
+++ wwbuild/docker-build-all.bat
@@ -1,4 +1,7 @@
 
 echo ""
 echo "Building source code using docker container"
-docker run -it --rm --name willywonka -v "%cd%\..:/WillyWonka" docker.usaust-edge.com/willywonka
+rem docker run -it --rm --name willywonka -v "%cd%\..:/WillyWonka" docker.usaust-edge.com/willywonka
+
+docker build -t willywonka . >nul
+docker run -it --rm --name willywonka -v "%cd%\..:/WillyWonka" willywonka
